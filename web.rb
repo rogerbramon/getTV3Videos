b391c1dd729115229de185e0b0ba65f8680a147d
Roger Bramon <rogerbramon@gmail.com>
2014-12-01 17:17:41 +0100

    ENH: Update webservice to use the new webservice of CCMA.

--- web.rb
+++ web.rb
@@ -6,50 +6,36 @@ require 'json'
 set :public_folder, Proc.new { File.join(root, "public") }
 
 get '/videos/:code' do
-    formatsURI = URI("http://www.tv3.cat/pvideo/FLV_bbd_dadesItem.jsp?idint=" + params[:code])
-
-    response = Hash.from_xml(Net::HTTP.get(formatsURI))["item"]
-
+    baseURL = "http://dinamics.ccma.cat/pvideo/media.jsp?media=video&version=0s&idint=" + params[:code] 
+    
+    response = JSON.parse(Net::HTTP.get(URI(baseURL + "&profile=pc")).force_encoding("iso-8859-1"))
+    
     outputVideos = []
-    if response["videos"]["video"].length > 0
-        video = response["videos"]["video"][0]
-
+    if !response.empty? && response["informacio"]["estat"]["actiu"]
+        
         # Video like tv3.cat
-        fileURI = URI("http://www.tv3.cat/pvideo/FLV_bbd_media.jsp?ID=" + params[:code] + "&QUALITY=" + video["qualitat"] + "&FORMAT=" + video["format"] + "&PROFILE=HTML5")
-        mediaURL = Hash.from_xml(Net::HTTP.get(fileURI))["tv3alacarta"]["item"]["media"]
+        videoPC = response["media"]
 
-        if mediaURL.is_a?(String) && !mediaURL.empty?
-            outputVideos << {
-                format: video["format"],
-                quality: "Mitjana",
-                url: mediaURL
-            }
-        end
+        outputVideos << {
+            format: videoPC["format"],
+            quality: "Mitjana",
+            url: videoPC["url"]
+        }
 
         # Video like TV3 Smart TV App
-        fileURI = URI("http://www.tv3.cat/pvideo/FLV_bbd_media.jsp?ID=" + params[:code] + "&FORMAT=" + video["format"] + "&PROFILE=iptv")
-        mediaHighData = Hash.from_xml(Net::HTTP.get(fileURI))["bbd"]["item"]["media"]
-        mediaHighURL = ""
+        videoTV = JSON.parse(Net::HTTP.get(URI(baseURL + "&profile=tv")))["media"]
 
-        if mediaHighData.is_a?(Array)
-            mediaHighURL = mediaHighData.grep(/media.tv3.cat/)
-        elsif mediaHighData.is_a?(String)
-            mediaHighURL = mediaHighData
-        end
-
-        if !mediaHighURL.empty?
-            outputVideos << {
-                format: video["format"],
-                quality: "Alta",
-                url: mediaHighURL
-            }
-        end
+        outputVideos << {
+            format: videoTV["format"],
+            quality: "Alta",
+            url: videoTV["url"]
+        }
     end
 
     output = {
-        title: response["title"],
-        description: response["desc"],
-        imgsrc: response["imgsrc"],
+        title: response["informacio"]["titol"],
+        description: response["informacio"]["descripcio"],
+        imgsrc: response["imatges"]["url"],
         videos: outputVideos
     }
 
@@ -61,3 +47,4 @@ end
 get '/' do
     send_file File.expand_path('index.html', settings.public_folder)
 end
+
