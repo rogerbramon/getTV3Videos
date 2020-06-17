aef0a3fb5c194e9b19f7ffba97664194a2ba6e16
Roger Bramon Feixas <rogerbramon@gmail.com>
2020-06-17 08:03:56 +0200

    Add HTML language

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import { GA_TRACKING_ID } from '../lib/gtag'
 export default class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang="ca">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
