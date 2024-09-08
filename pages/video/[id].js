40d3bad11bbc7b1af2c6011aec9565557416bc6d
Roger Bramon <rogerbramon@gmail.com>
2024-09-08 17:38:36 +0200

    Fix style

--- pages/video/[id].js
+++ pages/video/[id].js
@@ -57,67 +57,90 @@ export default function Video({ data }) {
     <>
       <Head>
         <title>
-          {
-            router.isFallback? 'Descarrega vídeos de TV3':
-            !data? 'Descarrega vídeos de TV3':
-            `Descarrega vídeos de TV3 - ${data.title}`
-          }
+          {router.isFallback
+            ? "Descarrega vídeos de TV3"
+            : !data
+            ? "Descarrega vídeos de TV3"
+            : `Descarrega vídeos de TV3 - ${data.title}`}
         </title>
-        {data? <meta name="description" content={data.description} /> : ''}
+        {data ? <meta name="description" content={data.description} /> : ""}
       </Head>
       <div>
-        {router.isFallback? <div>Cercant vídeos...</div>:
-        !data? <div>No s'ha trobat cap vídeo associat a aquesta adreça</div>:
-        <div className="videoCard" style={ {backgroundImage: `url('${data.imgsrc}')`} }>
-          <div className="info">
-            <h2>{data.title}</h2>
-            <p className="synopsis" dangerouslySetInnerHTML={parseDescription(data.description)}></p>
-            {
-              (data.videos.length > 0)?
-                <div className="links">
+        {router.isFallback ? (
+          <div>Cercant vídeos...</div>
+        ) : !data ? (
+          <div>No s'ha trobat cap vídeo associat a aquesta adreça</div>
+        ) : (
+          <div
+            className="videoCard"
+            style={{ backgroundImage: `url('${data.imgsrc}')` }}
+          >
+            <div className="info">
+              <h2>{data.title}</h2>
+              <p
+                className="synopsis"
+                dangerouslySetInnerHTML={parseDescription(data.description)}
+              ></p>
+              {data.videos.length > 0 ? (
+                <>
                   <h3>Vídeos</h3>
-                  {data.videos.sort(videosCompare).map((video) => {
-                    return (
-                      <a key={video.url} href={video.url}>{video.quality} <span>{video.format}</span></a>
-                    )
-                  })}
-                </div>
-              :''
-            }
-            {
-              (data.audioDescriptionVideos.length > 0)?
-                <div className="links">
+                  <div className="links">
+                    {data.videos.sort(videosCompare).map((video) => {
+                      return (
+                        <a key={video.url} href={video.url}>
+                          {video.quality} <span>{video.format}</span>
+                        </a>
+                      );
+                    })}
+                  </div>
+                </>
+              ) : (
+                ""
+              )}
+              {data.audioDescriptionVideos.length > 0 ? (
+                <>
                   <h3>Vídeos amb audiodescripció</h3>
-                  {data.audioDescriptionVideos.sort(videosCompare).map((video) => {
-                    return (
-                      <a key={video.url} href={video.url}>{video.quality} <span>{video.format}</span></a>
-                    )
-                  })}
-                </div>
-              :''
-            }
-            {
-              (data.subtitles.length > 0)?
-                <div className="links">
+                  <div className="links">
+                    {data.audioDescriptionVideos
+                      .sort(videosCompare)
+                      .map((video) => {
+                        return (
+                          <a key={video.url} href={video.url}>
+                            {video.quality} <span>{video.format}</span>
+                          </a>
+                        );
+                      })}
+                  </div>
+                </>
+              ) : (
+                ""
+              )}
+              {data.subtitles.length > 0 ? (
+                <>
                   <h3>Subtítols</h3>
-                  {data.subtitles.map((sub) => {
-                    return (
-                      <a key={sub.url} href={sub.url} download>{sub.text} <span>{sub.format}</span></a>
-                    )
-                  })}
-                </div>
-              : ''
-            }
+                  <div className="links">
+                    {data.subtitles.map((sub) => {
+                      return (
+                        <a key={sub.url} href={sub.url} download>
+                          {sub.text} <span>{sub.format}</span>
+                        </a>
+                      );
+                    })}
+                  </div>
+                </>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-        </div>
-        }
+        )}
       </div>
       <style jsx>{`
         .videoCard {
           background-image: url("https://statics.ccma.cat/multimedia/jpg/0/4/1584661891640.jpg");
           background-repeat: no-repeat;
           background-position: top right;
-          background-color:  black;
+          background-color: black;
           background-size: contain;
           background-color: linear-gradient;
           border-radius: 5px;
@@ -126,7 +149,12 @@ export default function Video({ data }) {
 
         .info {
           width: 100%;
-          background-image: linear-gradient(to top, black, black 85%, transparent);
+          background-image: linear-gradient(
+            to top,
+            black,
+            black 85%,
+            transparent
+          );
           color: white;
           padding: 24px;
           margin-top: 30vw;
@@ -136,7 +164,12 @@ export default function Video({ data }) {
           .info {
             min-height: 400px;
             width: 60%;
-            background-image: linear-gradient(to right, black, black 78%, transparent);
+            background-image: linear-gradient(
+              to right,
+              black,
+              black 78%,
+              transparent
+            );
             padding: 24px;
             padding-right: 48px;
             margin-top: 0;
@@ -160,11 +193,12 @@ export default function Video({ data }) {
         }
 
         .links {
-          margin-bottom: 30px;
+          margin-bottom: 18px;
+          display: flex;
+          flex-wrap: wrap;
         }
 
         .links a {
-          width: 150px;
           padding: 6px 12px;
           font-size: 0.9em;
           border-radius: 5px;
@@ -174,6 +208,7 @@ export default function Video({ data }) {
           margin-right: 12px;
           text-decoration: none;
           transition: all 0.2s ease;
+          margin-bottom: 12px;
         }
 
         .links a:hover {
@@ -184,8 +219,7 @@ export default function Video({ data }) {
         .links a span {
           color: gray;
         }
-
       `}</style>
     </>
-  )
+  );
 }
